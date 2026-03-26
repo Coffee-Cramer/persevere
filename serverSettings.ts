643bb33160ef5abd64229cbebfcfd45574d9549b
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-26 00:43:37 -0700

    Add files via upload

---
+++ serverSettings.ts
@@ -0,0 +1,78 @@
+/********************************************************************************
+* Race/Class Combo Selection:
+*       - See RaceClassData/raceClassCombos.ts
+*********************************************************************************/
+export const dwarfShaman: bool = false
+export const belfWarrior: bool = false
+export const undeadPaladin: bool = false
+export const gnomeHunter: bool = true
+export const humanHunter: bool = true
+export const trollWarlock: bool = true
+export const undeadHunter: bool = true
+export const dwarfMage: bool = true
+export const gnomePriest: bool = false
+export const dwarfWarlock: bool = false
+export const orcMage: bool = false
+export const gnomeDruid: bool = false 
+export const draeneiWarlock: bool = false
+
+
+/********************************************************************************
+* Enable/Disable Bow Skill for Night Elf Priests:
+*       - See RaceClassData/Races/NightElf/Priest.ts
+*********************************************************************************/
+export const priestBows: bool = true
+
+
+/********************************************************************************
+* Enable/Disable Blademaster Class:
+*       - See RaceClassData/CharClasses/blademaster/blademaster.ts
+*********************************************************************************/
+export const setBladeMaster: bool = true
+
+
+/********************************************************************************
+* Set Talents to 7 Rows, like Vanilla:
+*       - See RaceClassData/talentRevert.ts
+*********************************************************************************/
+export const talents60: bool = true
+
+
+/********************************************************************************
+* Enable/Disable Death Knight:
+*       - See RaceClassData/deathKnightEnable.ts
+*********************************************************************************/
+export const setDeathKnight: bool = false
+
+
+/********************************************************************************
+* Enable/Disable Troll Cannibalize:
+*       - See RaceClassData/Races/Troll/TrollCannibalizeData/trollCannibalize.ts
+*********************************************************************************/
+export const setTrollCannibalize: bool = true
+
+
+/********************************************************************************
+* Enable/Disable Level 60 Squish:
+*       - See NPCData/levelSquish.ts
+*********************************************************************************/
+export const setLevelSquish: bool = true
+
+
+/********************************************************************************
+* Enable/Disable Blackwing Lair:
+*       - See misc/blackWingLair.ts
+*********************************************************************************/
+export const setBWL: bool = false
+
+/********************************************************************************
+* Open/Close Gates of Ahn'Qiraj: (needs some touch ups)
+*       - See misc/ahnqiraj.ts
+*********************************************************************************/
+export const setAQ: bool = false
+
+/********************************************************************************
+* Enable/Disable TBC Races:
+*       - See content/NPCDATA/removeDraeneiBelf.ts
+*********************************************************************************/
+export const tbcRaceDelete = true
