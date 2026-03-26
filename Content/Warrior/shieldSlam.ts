c5c717c0104e29851ea179768bd2c81b95e80b90
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-26 00:36:42 -0700

    Delete Content/Warrior directory

--- Content/Warrior/shieldSlam.ts
+++
@@ -1,7 +0,0 @@
-import { DBC, std } from "wow/wotlk";
-
-const PROT_CAPSTONE = DBC.Talent.getRow(134)
-
-PROT_CAPSTONE.SpellRank.setIndex(0, 23922)
-
-//console.log(PROT_CAPSTONE.objectify())
--- Content/Warrior/victoryrush.ts
+++
@@ -1,4 +0,0 @@
-import { std } from "wow/wotlk";
-
-const VICTORYRUSH = std.Spells.load(34428)
-VICTORYRUSH.delete() //im sorry, child
