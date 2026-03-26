3f3ef1c0d6f120c30253590419e0cc85340df53a
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-26 00:53:56 -0700

    Delete Content/NPCData/Ghost_BleedImmunity.ts

--- Content/NPCData/Ghost_BleedImmunity.ts
+++
@@ -1,9 +0,0 @@
-import { std } from "wow/wotlk";
-
-const TMPLTS = std.CreatureTemplates
-
-TMPLTS.forEach((npc) => {
-    if(npc.TypeFlags.GHOST.get()){
-        npc.MechanicImmunity.BLEED.set(1)
-    }
-})
