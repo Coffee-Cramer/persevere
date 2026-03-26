870bdea988aca4f6cd6573771ffcac6d7c0150bd
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-26 00:52:39 -0700

    Add files via upload

---
+++ Content/NPCData/difficulty/beastAbilities.ts
@@ -0,0 +1,45 @@
+import { std } from "wow/wotlk";
+import { NEWRAGE } from "../../RaceClassData/CharClasses/blademaster/SpellVisuals/rage";
+import { NEWBLOODRAGE_VISUAL } from "../../RaceClassData/CharClasses/Warrior/SpellVisuals/bloodRageVisual";
+
+const NPCS = std.CreatureTemplates
+
+const BEASTRAGE = std.Spells.create('shrek6', 'beastrage')
+    .Name.enGB.set("Beastial Rage")
+    .AuraDescription.enGB.set("Attack Power increased.")
+    .Icon.setPath("ability_hunter_pet_wolf")
+    .Range.setSimple(0, 5)
+    .CastTime.setSimple(0)
+    .Duration.setSimple(0)
+    .Visual.set(std.Spells.load(18499).Visual.get())
+    .Duration.setSimple(8000)
+    .Effects.addMod(x => x
+        .Type.APPLY_AURA.set()
+        .Aura.MOD_ATTACK_POWER.set()
+        .PointsBase.set(1) //2
+        .PointsPerLevel.set(1) //1 per level
+        .ImplicitTargetA.UNIT_CASTER.set()
+    )
+    .Effects.addMod(x => x
+        .Type.APPLY_AURA.set()
+        .Aura.MOD_SCALE.set()
+        .PercentBase.set(9) //10%
+        .ImplicitTargetA.UNIT_CASTER.set()
+    )
+
+//console.log(BEASTRAGE.ID)
+
+const GGT = std.CreatureTemplates.load(547)
+GGT.InlineScripts.OnJustEnteredCombat((npc) => {
+    npc.CastSpell(npc, 81540, true)
+})
+
+const COYOTEPCKLDR = std.CreatureTemplates.load(833)
+COYOTEPCKLDR.InlineScripts.OnJustEnteredCombat((npc) => {
+    npc.CastSpell(npc, 81540, true)
+})
+
+const COYOTE = std.CreatureTemplates.load(834)
+COYOTE.InlineScripts.OnJustEnteredCombat((npc) => {
+    npc.CastSpell(npc, 81540, true)
+})
---
+++ Content/NPCData/difficulty/healthPools.ts
@@ -0,0 +1,27 @@
+import { std } from "wow/wotlk";
+
+const NPCS = std.CreatureTemplates
+
+NPCS.forEach((npc) => {
+    if(npc.Level.Max.get() > 1 && npc.Level.Max.get() <= 5){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.4)
+    }
+    else if(npc.Level.Min.get() >= 5 && npc.Level.Max.get() <= 10){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
+    }
+    else if(npc.Level.Min.get() >= 10 && npc.Level.Max.get() <= 20){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.35)
+    }
+    else if(npc.Level.Min.get() >= 20 && npc.Level.Max.get() <= 30){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.4)
+    }
+    else if(npc.Level.Min.get() >= 30 && npc.Level.Max.get() <= 40){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.45)
+    }
+    else if(npc.Level.Min.get() >= 40 && npc.Level.Max.get() <= 50){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.5)
+    }
+    else if(npc.Level.Min.get() >= 50 && npc.Level.Max.get() <= 60){
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.6)
+    }
+}) 
