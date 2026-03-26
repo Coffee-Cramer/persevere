c57591f76edac2b022529e64fede2751499c472a
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-26 00:36:19 -0700

    Add files via upload

---
+++ Content/Warrior/SpellVisuals/bloodRageVisual.ts
@@ -0,0 +1,18 @@
+import { std } from "wow/wotlk"
+
+const BLDRG = std.Spells.load(29131)
+const ENRGE = std.SpellVisuals.load(std.Spells.load(72147).Visual.get())
+const BLDRG_VISUAL = std.SpellVisuals.load(BLDRG.Visual.get())
+const FIRE = std.SpellVisuals.load(7553)
+
+const NEWENRGEKIT = std.SpellVisualKits.create(ENRGE.StateKit.get())
+NEWENRGEKIT.BaseEffect.set(5132) //fire base effect
+NEWENRGEKIT.LeftHandEffect.set(94) //rage hands
+NEWENRGEKIT.RightHandEffect.set(94) //rage hands
+
+    
+
+export const NEWBLOODRAGE_VISUAL = std.SpellVisuals.create(BLDRG_VISUAL.ID)
+    .StateKit.set(NEWENRGEKIT.ID) //state effects
+
+//BLDRG.Visual.set(NEWBLOODRAGE_VISUAL.ID)
---
+++ Content/Warrior/victoryrush.ts
@@ -0,0 +1,4 @@
+import { std } from "wow/wotlk";
+
+const VICTORYRUSH = std.Spells.load(34428)
+VICTORYRUSH.delete() //im sorry, child
