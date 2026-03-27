c7f679e97e8f00580bdaf49a5b864b0672739b76
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-27 16:43:16 -0700

    Revert starter gear to vanilla versions for classes
    Revert starter gear for various classes to a more vanilla version, adjusting items based on race and class.

--- Content/RaceClassData/StarterGear.ts
+++ Content/RaceClassData/StarterGear.ts
@@ -23,176 +23,6 @@ const SHAMAN = std.Classes.load('SHAMAN')
 const PALADIN = std.Classes.load('PALADIN')
 const DRUID = std.Classes.load('DRUID')
 
-//*************************************************************************************************************************
-//* Warlock
-//*************************************************************************************************************************
-for(let ClassRaceStartGear of WARLOCK.Races.get()){
-        ClassRaceStartGear.Outfits.both(gear => {
-            if(gear.Race.get() == ALLRACES_ENUM.HUMAN){
-                gear.Items.clearAll()
-                gear.Items.add(57) //robe
-                gear.Items.add(6097) //shirt
-                gear.Items.add(1396) //legs
-                gear.Items.add(59) //feet
-                gear.Items.add(2092) //dagger
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.GNOME){
-                gear.Items.clearAll()
-                gear.Items.add(57) //robe
-                gear.Items.add(6097) //shirt
-                gear.Items.add(1396) //legs
-                gear.Items.add(59) //feet
-                gear.Items.add(2092) //dagger
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.UNDEAD){
-                gear.Items.clearAll()
-                gear.Items.add(6129) //robe
-                gear.Items.add(1396) //legs
-                gear.Items.add(59) //feet
-                gear.Items.add(2092) //dagger
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.ORC){
-                gear.Items.clearAll()
-                gear.Items.add(6129) //robe
-                gear.Items.add(1396) //legs
-                gear.Items.add(59) //feet
-                gear.Items.add(2092) //dagger
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.TROLL){
-                gear.Items.clearAll()
-                gear.Items.add(6129) //robe
-                gear.Items.add(1396) //legs
-                gear.Items.add(59) //feet
-                gear.Items.add(2092) //dagger
-                gear.Items.add(6948) //hearthstone
-            }
-        })
-    }
-
-
-//*************************************************************************************************************************
-//* Priest
-//*************************************************************************************************************************
-for(let ClassRaceStartGear of PRIEST.Races.get()){
-        ClassRaceStartGear.Outfits.both(gear => {
-            if(gear.Race.get() == ALLRACES_ENUM.UNDEAD){
-                gear.Items.clearAll()
-                gear.Items.add(6144) //robe
-                gear.Items.add(53) //shirt
-                gear.Items.add(52) //legs
-                gear.Items.add(51) //feet
-                gear.Items.add(36) //worn mace
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.TROLL){
-                gear.Items.clearAll()
-                gear.Items.add(6144) //robe
-                gear.Items.add(53) //shirt
-                gear.Items.add(52) //legs
-                gear.Items.add(51) //feet
-                gear.Items.add(36) //worn mace
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.HUMAN){
-                gear.Items.clearAll()
-                gear.Items.add(6098) //robe
-                gear.Items.add(53) //shirt
-                gear.Items.add(52) //legs
-                gear.Items.add(51) //feet
-                gear.Items.add(36) //worn mace
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.DWARF){
-                gear.Items.clearAll()
-                gear.Items.add(6098) //robe
-                gear.Items.add(53) //shirt
-                gear.Items.add(52) //legs
-                gear.Items.add(51) //feet
-                gear.Items.add(36) //worn mace
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.NIGHTELF){ //Night Elf 
-                gear.Items.clearAll()
-                gear.Items.add(2504) //bow
-                gear.Items.add(2101) //quiver
-                gear.Items.add(2512) //arrows
-                gear.Items.add(6119) //chest
-                gear.Items.add(36) //worn mace
-                gear.Items.add(51) //boots
-                gear.Items.add(52) //pants
-                gear.Items.add(53) //shirt
-                gear.Items.add(6948) //hearth stone
-            }
-        })
-    }
-
-
-//*************************************************************************************************************************
-//* Warrior
-//*************************************************************************************************************************
-for(let ClassRaceStartGear of WARRIOR.Races.get()){
-        ClassRaceStartGear.Outfits.both(gear => {
-            if(gear.Race.get() == ALLRACES_ENUM.HUMAN){
-                gear.Items.clearAll()
-                gear.Items.add(38) //shirt
-                gear.Items.add(39) //legs
-                gear.Items.add(40) //feet
-                gear.Items.add(2362) //worn wooden shield
-                gear.Items.add(25) //worn short sword
-                gear.Items.add(6948) //hearthstone
-            }
-            if(gear.Race.get() == ALLRACES_ENUM.GNOME){
-                gear.Items.clearAll()
-                gear.Items.add(38) //shirt
-                gear.Items.add(39) //legs
-                gear.Items.add(40) //feet
-                gear.Items.add(2362) //worn wooden shield
-                gear.Items.add(25) //worn short sword
-                gear.Items.add(6948) //hearthstone
-            }
-            if(gear.Race.get() == ALLRACES_ENUM.NIGHTELF){
-                gear.Items.clearAll()
-                gear.Items.add(6120) //shirt
-                gear.Items.add(6121) //legs
-                gear.Items.add(6122) //feet
-                gear.Items.add(2362) //worn wooden shield
-                gear.Items.add(25) //worn short sword
-                gear.Items.add(6948) //hearthstone
-            }
-            if(gear.Race.get() == ALLRACES_ENUM.UNDEAD){
-                gear.Items.clearAll()
-                gear.Items.add(6125) //shirt
-                gear.Items.add(139) //legs
-                gear.Items.add(140) //feet
-                gear.Items.add(2362) //worn wooden shield
-                gear.Items.add(25) //worn short sword
-                gear.Items.add(6948) //hearthstone
-            }
-            if(gear.Race.get() == ALLRACES_ENUM.TROLL){
-                gear.Items.clearAll()
-                gear.Items.add(6125) //shirt
-                gear.Items.add(139) //legs
-                gear.Items.add(140) //feet
-                gear.Items.add(2362) //worn wooden shield
-                gear.Items.add(37) //worn axe
-                gear.Items.add(6948) //hearthstone
-            }
-            
-            if(gear.Race.get() == ALLRACES_ENUM.BLOODELF){
-                gear.Items.clearAll()
-                gear.Items.add(BELFWARR_SHIRT.ID) //shirt
-                gear.Items.add(BELFWARR_LEGS.ID) //legs
-                gear.Items.add(BELFWARR_BOOTS.ID) //feet
-                gear.Items.add(20849) //sword
-                gear.Items.add(20911) //shield
-                gear.Items.add(6948) //hearthstone
-            }
-        })
-    }
 
 
 //*************************************************************************************************************************
@@ -201,30 +31,6 @@ for(let ClassRaceStartGear of WARRIOR.Races.get()){
 for(let ClassRaceStartGear of HUNTER.Races.get()){
         ClassRaceStartGear.Outfits.both(gear => {
             if(gear.Race.get() == ALLRACES_ENUM.UNDEAD){
-                gear.Items.clearAll()
-                gear.Items.add(127) //shirt
-                gear.Items.add(6126) //legs
-                gear.Items.add(6127) //feet
-                gear.Items.add(37) //worn axe
-                gear.Items.add(6948) //hearthstone
-                gear.Items.add(2504) //bow
-                gear.Items.add(2101) //quiver
-                gear.Items.add(2512) //arrows
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.ORC){
-                gear.Items.clearAll()
-                gear.Items.add(127) //shirt
-                gear.Items.add(6126) //legs
-                gear.Items.add(6127) //feet
-                gear.Items.add(37) //worn axe
-                gear.Items.add(6948) //hearthstone
-                gear.Items.add(2504) //bow
-                gear.Items.add(2101) //quiver
-                gear.Items.add(2512) //arrows
-                gear.Items.add(6948) //hearthstone
-            }
-            else if(gear.Race.get() == ALLRACES_ENUM.TROLL){
                 gear.Items.clearAll()
                 gear.Items.add(127) //shirt
                 gear.Items.add(6126) //legs
@@ -266,7 +72,7 @@ for(let ClassRaceStartGear of HUNTER.Races.get()){
 
 
 //*************************************************************************************************************************
-//* Rogue
+//* Rogue (gives one dagger instead of 2, closer to vanilla's style)
 //*************************************************************************************************************************
 for(let ClassRaceStartGear of ROGUE.Races.get()){
         ClassRaceStartGear.Outfits.both(gear => {
@@ -310,16 +116,6 @@ for(let ClassRaceStartGear of ROGUE.Races.get()){
                 gear.Items.add(28979) //thrown
                 gear.Items.add(6948) //hearthstone
             }
-            else if(gear.Race.get() == ALLRACES_ENUM.BLOODELF){
-                gear.Items.clearAll()
-                gear.Items.add(20897) //shirt
-                gear.Items.add(20896) //legs
-                gear.Items.add(20898) //feet
-                gear.Items.add(20982) //dagger
-                gear.Items.add(6948) //hearthstone
-                gear.Items.add(28979) //thrown
-                gear.Items.add(6948) //hearthstone
-            }
         })
     }
 
@@ -348,9 +144,6 @@ for(let ClassRaceStartGear of NECRO.Races.get()){
             gear.Items.add(1395) //pants
             gear.Items.add(55) //boots
             gear.Items.add(2092) //dagger
-
-            if(gear.Race.get() == 1){
-                gear.Items.add(53) //shirt
-            }
+            gear.Items.add(53) //shirt
     })
 }
