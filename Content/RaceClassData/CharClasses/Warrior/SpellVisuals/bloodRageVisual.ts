dac0df2cb9a2910c0fe18872a868634844e35080
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-27 16:50:57 -0700

    Update bloodRageVisual.ts

--- Content/RaceClassData/CharClasses/Warrior/SpellVisuals/bloodRageVisual.ts
+++ Content/RaceClassData/CharClasses/Warrior/SpellVisuals/bloodRageVisual.ts
@@ -3,10 +3,10 @@ import { std } from "wow/wotlk"
 const BLDRG = std.Spells.load(29131)
 const ENRGE = std.SpellVisuals.load(std.Spells.load(72147).Visual.get())
 const BLDRG_VISUAL = std.SpellVisuals.load(BLDRG.Visual.get())
-const FIRE = std.SpellVisuals.load(7553)
+//const FIRE = std.SpellVisuals.load(7553)
 
 const NEWENRGEKIT = std.SpellVisualKits.create(ENRGE.StateKit.get())
-NEWENRGEKIT.BaseEffect.set(5132) //fire base effect
+//NEWENRGEKIT.BaseEffect.set(5132) //fire base effect
 NEWENRGEKIT.LeftHandEffect.set(94) //rage hands
 NEWENRGEKIT.RightHandEffect.set(94) //rage hands
 
