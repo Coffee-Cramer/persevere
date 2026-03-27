8a05e6bbaa2a85f3d027436137ba5ec7ce5eb4fb
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-27 16:34:45 -0700

    Delete Content/NPCData/ZONES/Undercity/deletesDarkRAngers.ts

--- Content/NPCData/ZONES/Undercity/deletesDarkRAngers.ts
+++
@@ -1,6 +0,0 @@
-import { std } from "wow/wotlk";
-
-//why? Goblin patch messes up their hoods
-std.CreatureTemplates.load(36224).delete()
-std.CreatureTemplates.load(36225).delete()
-std.CreatureTemplates.load(36226).delete()
