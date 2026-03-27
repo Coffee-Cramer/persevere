098fc4582e136fb7507cd502f1db785a97fa4362
Cwamey <98248289+Coffee-Cramer@users.noreply.github.com>
2026-03-27 16:59:26 -0700

    Standardize health modification for NPC levels

--- Content/NPCData/difficulty/healthPools.ts
+++ Content/NPCData/difficulty/healthPools.ts
@@ -4,24 +4,24 @@ const NPCS = std.CreatureTemplates
 
 NPCS.forEach((npc) => {     //0.3 seems to be the sweet spot
     if(npc.Level.Max.get() > 1 && npc.Level.Max.get() <= 5){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.4)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 5 && npc.Level.Max.get() <= 10){
         npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 10 && npc.Level.Max.get() <= 20){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.35)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 20 && npc.Level.Max.get() <= 30){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.4)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 30 && npc.Level.Max.get() <= 40){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.45)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 40 && npc.Level.Max.get() <= 50){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.5)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
     else if(npc.Level.Min.get() >= 50 && npc.Level.Max.get() <= 60){
-        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.6)
+        npc.Stats.HealthMod.set(npc.Stats.HealthMod.get() + 0.3)
     }
 }) 
